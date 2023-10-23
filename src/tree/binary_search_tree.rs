fa41b0a33feafbc24c390e0c2d3004d30efb5333
N0I0C0K <nick131410@aliyun.com>
2023-10-23 16:10:30 +0800

    feat：添加二叉查找树

--- src/tree/binary_search_tree.rs
+++ src/tree/binary_search_tree.rs
@@ -15,7 +15,7 @@ impl<T> TreeNode<T> {
 }
 
 struct BinarySearchTree<T> {
-    head: Option<TreeNode<T>>,
+    head: Option<Box<TreeNode<T>>>,
 }
 
 impl<T> BinarySearchTree<T>
@@ -26,9 +26,48 @@ where
         BinarySearchTree { head: None }
     }
 
+    fn lower_bound(&self, val: &T) -> Option<&TreeNode<T>>
+    where
+        T: PartialOrd,
+    {
+        if self.head.is_none() {
+            return None;
+        }
+        let mut t_node = self.head.as_ref();
+        let mut res = None::<&TreeNode<T>>;
+        while let Some(node) = t_node {
+            if *val < node.val {
+                res = Some(node);
+                t_node = node.left.as_ref();
+            } else if *val > node.val {
+                res = Some(node);
+                t_node = node.right.as_ref();
+            } else {
+                res = Some(node);
+                break;
+            }
+        }
+        res
+    }
+
+    fn upper_bound(&self, val: &T) -> Option<&TreeNode<T>>
+    where
+        T: PartialOrd,
+    {
+        return self.lower_bound(val).map_or(None, |x| {
+            if x.val == *val {
+                return match x.right.as_ref() {
+                    Some(ri) => Some(ri),
+                    None => None,
+                };
+            }
+            return Some(x);
+        });
+    }
+
     fn insert(&mut self, val: T) {
         if self.head.is_none() {
-            self.head = Some(TreeNode::new(val));
+            self.head = Some(Box::new(TreeNode::new(val)));
             return;
         }
         let mut node = self.head.as_mut().unwrap();
@@ -49,5 +88,51 @@ where
                 }
             }
         }
+    }
+
+    fn contain(&self, val: &T) -> bool
+    where
+        T: PartialOrd,
+    {
+        let node = self.lower_bound(val);
+        return node.is_some_and(|x| x.val == *val);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::BinarySearchTree;
+
+    #[test]
+    fn test() {
+        let mut tree: BinarySearchTree<i32> = BinarySearchTree::new();
+        assert!(!tree.contain(&10));
+        tree.insert(10);
+        assert!(tree.contain(&10));
+        tree.insert(11);
+        tree.insert(2);
+        tree.insert(12);
+        tree.insert(213);
+        tree.insert(7);
+
+        let t = tree.lower_bound(&3);
+
+        assert!(t.is_some_and(|x| { x.val == 7 }));
+
+        let t = tree.lower_bound(&11);
+
+        assert!(t.is_some_and(|x| { x.val == 11 }));
+
+        let t = tree.upper_bound(&11);
+
+        assert!(t.is_some_and(|x| { x.val == 12 }));
+
+        let t = tree.upper_bound(&213);
+
+        assert!(t.is_none());
+
+        let t = tree.upper_bound(&-121);
+
+        assert!(t.is_some_and(|x| { x.val == 2 }));
     }
 }
